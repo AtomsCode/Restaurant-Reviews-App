45a92727e3043d7144d9c73551c9d3c0adc39f17
AtomCode <mr.ali.alhelal@gmail.com>
2019-06-29 01:10:28 +0300

    docs + style : add media query + meta tags

--- js/main.js
+++ js/main.js
@@ -68,7 +68,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoiYXRvbWNvZGUiLCJhIjoiY2p4Z212MGs3MHJrMTN0cGp3d2NrMHZndiJ9.whs7rY7Rg3eOIjs-6wz15A',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
